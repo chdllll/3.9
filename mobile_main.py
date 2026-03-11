e23d305d11bd88bf5b246a81a3d0ce514b35e583
chdllll <1956653715@qq.com>
2026-03-11 22:51:59 +0800

    Update mobile_main.py

--- mobile_main.py
+++ mobile_main.py
@@ -4,7 +4,7 @@ import json
 import asyncio
 from threading import Thread
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, BASE_DIR)
 
 def is_android():
